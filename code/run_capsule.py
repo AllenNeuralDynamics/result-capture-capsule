fec84bfb3ef389a9f8727672dbfa066994fb7239
bjhardcastle <ben.hardcastle@alleninstitute.org>
2026-06-05 12:43:38 -0700

    Don't skip errored results (can be toggled in Automation on capsule)

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -8,10 +8,9 @@ logging.basicConfig(level=logging.INFO, force=True)
 def main():
     computation_params = utils.ComputationParams()
     if computation_params.co_source_exit_code != 0:
-        logger.error(f"Source computation {computation_params.co_source_computation_id} exited with non-zero exit code {computation_params.co_source_exit_code}. Skipping post-run processing.")
-        return
+        logger.warning(f"Source computation {computation_params.co_source_computation_id} exited with exit code {computation_params.co_source_exit_code}.")
     data_asset_params = utils.get_data_asset_params_model(computation_params.co_source_computation_id)
     utils.create_data_asset(data_asset_params)
-    
+
 if __name__ == "__main__": 
     main()
